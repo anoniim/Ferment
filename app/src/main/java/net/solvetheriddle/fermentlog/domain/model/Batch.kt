8a161bd3fb485688fcdf9747d79d7735019a60a8
Marcel Bro <marcel.broz@gmail.com>
2025-06-18 01:10:31 +0200

    Active bathes view improved

--- app/src/main/java/net/solvetheriddle/fermentlog/domain/model/Batch.kt
+++ app/src/main/java/net/solvetheriddle/fermentlog/domain/model/Batch.kt
@@ -1,20 +1,27 @@
 package net.solvetheriddle.fermentlog.domain.model
 
+import android.os.Build
 import java.time.LocalDate
-import java.time.ZoneId
-import java.util.Date
 import java.util.UUID
 
 data class Batch(
     val id: String = UUID.randomUUID().toString(),
-    val name: String,
+    val name: String? = null,
     val status: Status = Status.ACTIVE,
     val phase: BrewingPhase = BrewingPhase.PRIMARY,
     val startDate: LocalDate,
     val vessel: Vessel,
-    val ingredients: List<IngredientAmount> = emptyList(),
+    val primaryIngredients: List<IngredientAmount> = emptyList(),
+    val secondaryIngredients: List<IngredientAmount> = emptyList(),
     val parentId: String? = null
-)
+) {
+    val brewDurationInDays: Int =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            startDate.datesUntil(LocalDate.now()).count().toInt()
+        } else {
+            TODO("VERSION.SDK_INT < UPSIDE_DOWN_CAKE") // Increase min SDK version and REMOVE THIS CODE
+        }
+}
 
 enum class Status {
     UNDEFINED,
